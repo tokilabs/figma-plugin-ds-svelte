9e52d6b8520298ba8099c674cb79e2812ca9e34f
Yago Fernandes <yagoalmeida@gmail.com>
2023-05-17 16:51:19 -0300

    Initiates migration to TS

---
+++ rollup.config.mjs
@@ -0,0 +1,34 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import svg from "rollup-plugin-svg";
+import postcss from "rollup-plugin-postcss";
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+	input: "src/test.ts",
+	output: {
+		file: `public/components.js`,
+		format: "iife",
+		name: "components",
+	},
+	plugins: [
+		svelte({
+			include: ["src/**/*.svelte", "node_modules/**/src/*.svelte"],
+		}),
+		resolve(),
+		commonjs(),
+		svg(),
+		postcss({
+			plugins: [],
+		}),
+		!production && serve("public"),
+		!production && livereload("public"),
+	],
+	watch: {
+		clearScreen: false,
+	},
+};
